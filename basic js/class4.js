cdd1a7768b41e64f9ec6c80a14beab2b08a80c5b
Amit Kumar <amitkumar.developer1@gmail.com>
2021-03-16 17:53:04 +0530

    Upload

---
+++ basic js/class4.js
@@ -0,0 +1,23 @@
+const a = 'teenage';
+switch (a) {
+	case 'children':
+		console.log('Its children');
+		break;
+	case 'teenage':
+		console.log('its a teenage');
+		break;
+	case 'adult':
+		console.log('its a adult');
+		break;
+	case 'oldage':
+		console.log('its a oldage');
+		break;
+	default:
+		console.log('insert valid input');
+}
+
+var i = 1;
+while (i < 10) {
+	console.log('loop is running');
+	i = i + 1;
+}
---
+++ basic js/script.js
@@ -0,0 +1,60 @@
+// js
+
+var age = 35;
+var email = 'amit@gmail.com';
+var height = 6.3;
+var isOldAge = false;
+
+// console.clear()
+// console.log('hello world')
+// console.log(age)
+// console.log('Your age is', age)
+// console.error('error in age', age)
+// console.info('successfull login')
+// console.warn('gender is mandatory')
+
+// program to check whether a person is old age or not
+
+if (age <= 18) {
+	console.log('Teenage');
+} else if (age > 18 && age <= 60) {
+	console.log('adult');
+} else {
+	console.log('old age');
+}
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// make a table of 2
+var i = 1;
+for (i = 1; i <= 10; i++) {
+	console.log('2 x ', i, ' = ', 2 * i);
+}
+i += 1;
+i = i + 1;
+
+i = 10;
+// console.log(i++) = 10
+console.log(++i) = 11;
+
+// python
+// age = 23
+// email = 'amit@gmail.com'
+// height = 6.3
+// isOldAge = false
+// if (age < 60):
+//     print('Not old')
+// else :
+//     print('old age')
+// elif
+// for i in range(0, 10, 1):
+//     print(i)
+
+// table
+// palindrome
+// factorial
+// calculator
+// odd even
+// prime number
